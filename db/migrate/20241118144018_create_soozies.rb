c3f5de9db3da441fde2f51ce4380717d30c15da1
Erika Belicova <erika.belicov@gmail.com>
2024-11-18 16:11:51 +0100

    Added devise user and soozie model, controller and routes

---
+++ db/migrate/20241118144018_create_soozies.rb
@@ -0,0 +1,21 @@
+class CreateSoozies < ActiveRecord::Migration[7.1]
+  def change
+    create_table :soozies do |t|
+      t.string :first_name
+      t.string :last_name
+      t.text :description
+      t.string :gender
+      t.string :address
+      t.boolean :available
+      t.integer :price
+      t.date :birthdate
+      t.string :hair_color
+      t.string :eye_color
+      t.integer :height
+      t.integer :weight
+      t.references :user_id, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
