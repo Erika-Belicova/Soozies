4d89c04719cc75813cb33204496bab324af94305
Erika Belicova <erika.belicov@gmail.com>
2024-11-18 16:24:14 +0100

    Fixed user_id to user in soozies schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_134546) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_144018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "soozies", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "description"
+    t.string "gender"
+    t.string "address"
+    t.boolean "available"
+    t.integer "price"
+    t.date "birthdate"
+    t.string "hair_color"
+    t.string "eye_color"
+    t.integer "height"
+    t.integer "weight"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_soozies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +45,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_134546) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "soozies", "users"
 end
