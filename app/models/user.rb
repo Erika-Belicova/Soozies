295527e511ac4f3ad2ddda0b47f051406f018355
Erika Belicova <erika.belicov@gmail.com>
2024-11-19 17:12:18 +0100

    Controller fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :soozies, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :offers, through: :soozies, source: :bookings
+
   has_one_attached :photo
 end
