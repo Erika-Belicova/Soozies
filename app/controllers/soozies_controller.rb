13c5bfd686a66243ca654093d194ad3badf3918b
Erika Belicova <erika.belicov@gmail.com>
2024-11-19 12:35:55 +0100

    Changed columns in database soozies, added controller soozies

--- app/controllers/soozies_controller.rb
+++ app/controllers/soozies_controller.rb
@@ -1,2 +1,49 @@
 class SooziesController < ApplicationController
+  before_action :set_soozie, only: [ :show, :edit, :update ]
+
+  def index
+    @soozies = Soozie.all
+  end
+
+  def show
+  end
+
+  def new
+    @soozie = Soozie.new
+  end
+
+  def create
+    @soozie = Soozie.new(soozie_params)
+    if @sozie.save
+      redirect_to soozies_path(@soozie), notice: "Soozie created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @soozie.update(soozie_params)
+    if @sozie.save
+      redirect_to soozies_path(@soozie), notice: "Soozie created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_soozie
+    @soozie = Soozie.find(params[:id])
+  end
+
+  def soozie_params
+    params.require(@soozie).permit(
+      :first_name, :last_name, :description, :gender, :city, # migration ! not changed in database yet
+      :available, :price, :age, # migration ! not changed in database yet
+      :hair_color, :eye_color, :height, :weight, :user
+    )
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_154854) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_113138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,10 +31,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_154854) do
     t.string "last_name"
     t.text "description"
     t.string "gender"
-    t.string "address"
+    t.string "city"
     t.boolean "available"
     t.integer "price"
-    t.date "birthdate"
     t.string "hair_color"
     t.string "eye_color"
     t.integer "height"
@@ -42,6 +41,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_154854) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
     t.index ["user_id"], name: "index_soozies_on_user_id"
   end
 
