3d9db171db72bad3bc57c4f59ec7fac49b186257
Benoit Lamy <lamybenoit@gmail.com>
2024-11-20 12:44:37 +0100

    adding use profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def show
+    @user = current_user
+    @soozies = current_user.soozies
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to user_path(@user), notice: "User updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :email, :first_name, :last_name, :description)
+  end
 end
