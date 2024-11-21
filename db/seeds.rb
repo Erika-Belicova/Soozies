41bc5381df770f870afe59b9190ca173df07f472
exec zsh <maudine83@hotmail.com>
2024-11-21 18:21:10 +0100

    user-show added

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,13 @@ Rails.root.join("db/seeds/users").children.each do |filepath|
   username = filepath.to_s.split("/")[-1].split(".")[0]
   file = File.open(filepath)
   puts "Creating user #{username}"
-  user = User.new(email: "#{username}@example.com", password: "password")
+  user = User.new(
+     email: "#{username}@example.com",
+     password: "password",
+     name: Faker::Name.name,
+     city: Faker::Address.city,
+     username: "#{username}"
+    )
   user.photo.attach(io: file, filename: "#{username}.jpg", content_type: "image/jpg")
   user.save
 end
