ea31734e541d6d1d9045997b69b688195637112a
Wanda Valadares WandaWanda1@users.noreply.github.com <wandawanda@gmail.com>
2024-11-22 12:10:25 +0100

    added range to the calendar

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,12 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
-
 import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
+import rangePlugin from "flatpickrRangePlugin";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = [ 'startDateInput', 'endDateInput' ]
+// On controller's connection, call the flatpickr
+// function in order to build the calendars
   connect() {
-    flatpickr(this.element, {
+    flatpickr(this.startDateInputTarget, {
       minDate: "today",
-    });
+      plugins: [new rangePlugin({ input: this.endDateInputTarget})]
+    })
   }
 }
